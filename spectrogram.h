1bde4dbff03dd2446cb2529927bf6eddc08c7a15
Farzan Mirshekari <farzanmirshekari@gmail.com>
2023-06-03 13:50:12 -0400

    Spectrogram struct header..

---
+++ spectrogram.h
@@ -0,0 +1,22 @@
+#ifndef SPECTROGRAM_H
+#define SPECTROGRAM_H
+
+#include <fftw3.h>
+
+typedef struct {
+    int length;
+    fftw_plan plan;
+
+    double *window;
+    double *time_domain;
+    double *frequency_domain;
+    double *magnitude;
+
+    double data[];
+} Spectrogram;
+
+Spectrogram * intialize( int length );
+void destroy( Spectrogram *spectrogram );
+double get_magnitude( Spectrogram *spectrogram );
+
+#endif
